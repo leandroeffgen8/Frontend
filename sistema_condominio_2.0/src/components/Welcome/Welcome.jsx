ae4364cfa074938a1509e5d9e62d9ea3605e3457
leandroeffgen8 <70702672+leandroeffgen8@users.noreply.github.com>
2024-10-15 10:39:39 -0300

    Add files via upload

---
+++ sistema_condominio_2.0/src/components/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import './Welcome.css'
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+        <h2 style={{ whiteSpace: 'pre-line' }}>Faça o login ou {'\n'} cadastra-se em nosso site.</h2>
+    </div>
+  )
+}
+
+export default Welcome
